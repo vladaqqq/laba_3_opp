38181611bc11b7fe187a7711067a665cd88e2e31
vlad4 <vlad4.efremova@yandex.ru>
2024-12-11 23:47:57 +0300

    тест функций для файла и ссылки

--- test_main.py
+++ test_main.py
@@ -16,4 +16,14 @@ class TestCheckAll(unittest.TestCase):
             with self.subTest(i=i):
                 self.assertEqual(check_string(password=i), f"Пароли не найдены")
 
+    def test_check_file(self):
+        self.assertEqual(check_file(path="example.txt"), "Все найденные пароли: ['aA!12345']")
 
+    def test_check_url(self):
+        example_html = "<li>Valid password:<strong> StrongP@ss1 </strong><rd:<strong> Test@123A </strong></li>"
+        self.assertEqual(check_string(password=example_html),
+                         "Все найденные пароли: ['StrongP@ss1', 'Test@123A']")
+
+
+if __name__ == "__main__":
+    unittest.main()
